cacc205819fa1687bca35d578c59bd4543d199e3
shamveelkhan <82874369+shamveelkhan@users.noreply.github.com>
2025-01-07 23:07:06 +0500

    Update receiveFiles.js

--- frontend/receiveFiles/receiveFiles.js
+++ frontend/receiveFiles/receiveFiles.js
@@ -1,3 +1,38 @@
+const BASE_URL='https://httpbin.org/post';
+const form = document.querySelector('form');
+const contactButton=document.getElementById('contactButton');
+const contactDiv=document.getElementById('contact');
+const fileInput=document.getElementById('file-input');
+
+function downloadFile(base64String, fileName, fileType) {
+
+    const base64WithoutPrefix = base64String.replace(/^data:.*,/, '');
+
+    const binaryData = atob(base64WithoutPrefix);
+ 
+    const bytes = new Uint8Array(binaryData.length);
+    for (let i = 0; i < binaryData.length; i++) {
+        bytes[i] = binaryData.charCodeAt(i);
+    }
+
+    const blob = new Blob([bytes], { type: fileType });
+    const link = document.createElement('a');
+    link.href = URL.createObjectURL(blob);
+    link.download = fileName;
+
+    link.click();
+    URL.revokeObjectURL(link.href);
+}
+
+contactButton.addEventListener('click',()=> {
+    if(contactDiv.style.display==='none') {
+        contactDiv.style.display='block';
+    }
+    else {
+        contactDiv.style.display='none';
+    }
+})
+
 document.getElementById('menu-toggle').addEventListener('click', () => {
     document.getElementById('sidenav').style.width = '250px';
 });
@@ -8,4 +43,18 @@ document.getElementById('close-nav').addEventListener('click', () => {
 
 document.getElementById('homeButton').addEventListener('click',() => {
     window.location.href='../index.html';
+})
+
+form.addEventListener('submit',async(e) => {
+    e.preventDefault();
+    const file=fileInput.files[0];
+    let DATA = new FormData(form);
+    DATA.append('filename',file.name);
+    DATA.append('filetype',file.type);
+    let response = await fetch(BASE_URL, {
+        method: 'POST',
+        body: DATA
+    })
+    let result=await response.json();
+    console.log(await result);
 })
