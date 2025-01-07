0c15b6cdaf998ea6527d51e5980018826e65808a
shamveelkhan <82874369+shamveelkhan@users.noreply.github.com>
2025-01-07 23:03:44 +0500

    Update main.js

--- frontend/main.js
+++ frontend/main.js
@@ -1,3 +1,26 @@
+const contactButton=document.getElementById('contactButton');
+const contactDiv=document.getElementById('contact');
+
+contactButton.addEventListener('click',()=> {
+    if(contactDiv.style.display==='none') {
+        contactDiv.style.display='block';
+    }
+    else {
+        contactDiv.style.display='none';
+    }
+})
+
+document.querySelectorAll('.icons')[0].addEventListener('click',() => {
+    window.open('https://github.com/shamveelkhan?tab=overview&from=2025-01-01&to=2025-01-07','_blank')
+})
+
+document.querySelectorAll('.icons')[1].addEventListener('click',() => {
+    window.open('https://www.linkedin.com/in/shamveel-khilji-78a74a345?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app','_blank');
+})
+
+document.querySelectorAll('.icons')[2].addEventListener('click',() => {
+    window.open('https://x.com/KhiljiSham34999','_blank');
+})
 
 document.getElementById('menu-toggle').addEventListener('click', () => {
     const sidenav = document.getElementById('sidenav');
