e8ad433e5afac01b8d41b0252006a0cbb392fbd0
shamveelkhan <82874369+shamveelkhan@users.noreply.github.com>
2025-01-07 23:09:25 +0500

    Update sendFiles.js

--- frontend/sendFiles/sendFiles.js
+++ frontend/sendFiles/sendFiles.js
@@ -1,6 +1,21 @@
 const BASE_URL='https://httpbin.org/post';
 const FORM = document.querySelector('form');
+const dropZone = document.getElementById("dropbox");
+const fileInput=document.getElementById('file-input');
+const dropText =document.getElementById('dropText');
+const contactButton=document.getElementById('contactButton');
+const contactDiv=document.getElementById('contact');
+let selectedFile = null;
 let fileCount =0;
+
+contactButton.addEventListener('click',()=> {
+    if(contactDiv.style.display==='none') {
+        contactDiv.style.display='block';
+    }
+    else {
+        contactDiv.style.display='none';
+    }
+})
 
 document.getElementById('menu-toggle').addEventListener('click', () => {
     document.getElementById('sidenav').style.width = '250px';
@@ -14,11 +29,59 @@ document.getElementById('close-nav').addEventListener('click', () => {
 
 document.getElementById('homeButton').addEventListener('click',() => {
     window.location.href='../index.html'
+});
+
+document.querySelectorAll('.icons')[0].addEventListener('click',() => {
+    window.open('https://github.com/shamveelkhan?tab=overview&from=2025-01-01&to=2025-01-07','_blank')
 })
+
+document.querySelectorAll('.icons')[1].addEventListener('click',() => {
+    window.open('https://www.linkedin.com/in/shamveel-khilji-78a74a345?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app','_blank');
+})
+
+document.querySelectorAll('.icons')[2].addEventListener('click',() => {
+    window.open('https://x.com/KhiljiSham34999','_blank');
+})
+
+document.querySelectorAll('.icons')[3].addEventListener('click',() => {
+    window.open('https://github.com/shamveelkhan?tab=overview&from=2025-01-01&to=2025-01-07','_blank')
+})
+
+document.querySelectorAll('.icons')[4].addEventListener('click',() => {
+    window.open('https://www.linkedin.com/in/shamveel-khilji-78a74a345?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app','_blank');
+})
+
+document.querySelectorAll('.icons')[5].addEventListener('click',() => {
+    window.open('https://x.com/KhiljiSham34999','_blank');
+})
+
+fileInput.addEventListener('change',() => {
+    const file = fileInput.files[0];
+    dropText.innerHTML=file.name;
+})
+
+dropZone.addEventListener("dragover", (e) => {
+    e.preventDefault();
+});
+
+dropZone.addEventListener("drop", (e) => {
+    e.preventDefault();
+
+    const files = e.dataTransfer.files;
+    const fileInput = document.getElementById('file-input');
+
+    if (files.length === 0) return;
+
+    selectedFile = files[0];
+    const dataTransfer = new DataTransfer();
+    dataTransfer.items.add(selectedFile);
+    fileInput.files = dataTransfer.files;
+    const file = fileInput.files[0];
+    dropText.innerHTML=file.name;
+});
 
 FORM.addEventListener('submit',async(e)=> {
     e.preventDefault();
-    const fileInput = document.getElementById('file-input');
     const file = fileInput.files[0];
     
     if(!file) {
@@ -55,7 +118,7 @@ FORM.addEventListener('submit',async(e)=> {
             }
         
         catch(Err) {
-                console.log('error was: ',Err)
+            console.log('error was: ',Err)
         }
     }
 })
